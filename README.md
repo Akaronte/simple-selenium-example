416daefe85ad34228870641a0977619ddb034c9f
Akaronte <akaronte@gmail.com>
2022-08-22 01:21:04 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple selenium maven
+
+mvn dependency:sources 
+
+mvn package
+
+java -jar ./target/selenium-1.0-SNAPSHOT.jar
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.awt.Robot;
+import java.awt.AWTException;
+
+public class App 
+{
+    public static void main(String[] args) {
+        // ChromeOptions options = new ChromeOptions();
+        // options.addArguments("start-maximized");
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://piensoluegoinstalo.com");
+
+        try {
+            Robot robot = new Robot();
+            robot.mouseMove(300, 600);
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+
+        //driver.quit();
+    }
+}
