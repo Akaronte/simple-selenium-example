b2bbebeb46663b570aeb75525b5b5ae7bd703c4e
Akaronte <akaronte@gmail.com>
2022-08-22 02:41:55 +0200

    network-listener

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,16 +3,28 @@ package org.example;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.v103.network.Network;
 
 import java.awt.Robot;
 import java.awt.AWTException;
+import java.util.Optional;
 
 public class App 
 {
     public static void main(String[] args) {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
         ChromeDriver driver = new ChromeDriver(options);
+        DevTools devTools = driver.getDevTools();
+        devTools.createSession();
+        devTools.send(Network.enable(Optional.empty(),Optional.empty(),Optional.empty()));
+        devTools.addListener(Network.requestWillBeSent(), request -> {
+            System.out.println("Request URL: "+request.getRequest().getUrl());
+            System.out.println("Request METHOD: "+request.getRequest().getMethod());
+            System.out.println("Request HEADERS: "+request.getRequest().getHeaders());
+        });
 
         driver.get("https://piensoluegoinstalo.com");
 
